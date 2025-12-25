d8c86ec0870da9ea25b6c494b132dfb970630742
yong geon <gunnylove@gmail.com>
2025-12-25 16:02:39 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // 정적 파일 서빙을 위한 설정
-  async rewrites() {
-    return [
-      {
-        source: '/dashboard',
-        destination: '/dashboard/index.html',
-      },
-      {
-        source: '/trade',
-        destination: '/trade/index.html',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
